dc5c103a8b7323c92bc8f42184a4577002605821
sravanthi <sravanthikamani@gmail.com>
2024-05-15 17:40:11 +0000

    Solution

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className='backgroundColor'>
+    <h1 className='heading'>Super Over League</h1>
+    <div className='imageContainer'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        class='image'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        class='image'
+      />
+    </div>
+  </div>
+)
+ReactDOM.render(element, document.getElementById('root'))
